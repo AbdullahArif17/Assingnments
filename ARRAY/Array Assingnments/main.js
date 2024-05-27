41e94f190e414045adc1b7de7c1566a16b1b9649
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-05-27 21:34:11 +0500

    update

--- ARRAY/Array Assingnments/main.js
+++ ARRAY/Array Assingnments/main.js
@@ -1,19 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let product = {
-    name: 'ABC',
+const product1 = {
+    name: 'T-Shirt',
     price: 2000,
     inventory: {
         stock: 100,
-        colorOptions: ['White', 'Black', 'Red', 'Blue']
-    },
+        colorOptions: ['red', 'blue', 'green']
+    }
 };
-function changeColor(product, newColor) {
-    product.inventory.colorOptions.push(newColor);
-    if (newColor === "Red") {
-        product.price *= 1.10;
+const product2 = {
+    name: 'Jeans',
+    price: 4000,
+    inventory: {
+        stock: 50,
+        colorOptions: ['blue', 'black', 'white']
     }
-    else if (newColor === "Blue") {
-        product.price *= 0.95;
+};
+const product3 = {
+    name: 'Sneakers',
+    price: 6000,
+    inventory: {
+        stock: 75,
+        colorOptions: ['white', 'black', 'red']
     }
-}
+};
+const products = [product1, product2, product3];
+let colorChange = (products, newColor) => {
+    products['inventory'].colorOptions === newColor;
+    if (newColor == 'red') {
+        products.price = products.price + (products.price / 100) * 10;
+    }
+    else if (newColor == 'black') {
+        products.price = products.price + (products.price / 100) * 15;
+    }
+    else if (newColor == 'white') {
+        products.price = products.price + (products.price / 100) * 5;
+    }
+    else if (newColor == 'blue') {
+        products.price = products.price + (products.price / 100) * 10;
+    }
+    return products;
+};
+console.log(colorChange(product1, 'black'));
+console.log(colorChange(product2, 'white'));
+console.log(colorChange(product3, 'red'));
--- ARRAY/Array Assingnments/main.ts
+++ ARRAY/Array Assingnments/main.ts
@@ -21,7 +21,7 @@ const product2:Product = {
       price: 40.00,
       inventory: {
         stock: 50,
-        colorOptions: ['blue', 'black']
+        colorOptions: ['blue', 'black', 'white']
       }
     }
 const product3:Product = {
@@ -32,6 +32,27 @@ const product3:Product = {
         colorOptions: ['white', 'black', 'red']
       }
     }
+
+    const products = [product1,product2,product3]
+
+    let colorChange =  (products: Product, newColor:any) => {
+      products['inventory'].colorOptions === newColor
+      if(newColor == 'red' ){
+        products.price = products.price + (products.price/ 100) * 10
+      }else if(newColor == 'black'){
+        products.price = products.price + (products.price/ 100) * 15
+      }else if(newColor == 'white'){
+        products.price = products.price + (products.price/ 100) * 5
+      }else if(newColor == 'blue'){
+        products.price = products.price + (products.price/ 100) * 10
+      }
+      return products;
+    }
+
+    console.log(colorChange(product1 ,'black'))
+    console.log(colorChange(product2 ,'white'))
+    console.log(colorChange(product3 ,'red'))
+
 
    
 
